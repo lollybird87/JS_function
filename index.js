c028c718ab6013adf81f1a6aca997c8bebf5908c
Oksana Kozlova <lollybird87@gmail.com>
2020-06-18 02:04:34 +0300

    Add area calculation functions

--- index.js
+++ index.js
@@ -29,3 +29,50 @@ function checkMultiplicity(a , b) {
 function checkTriangle(a , b, c) {
     return a + b > c && a + c > b && b + c > a ;
 }
+
+
+/**
+ *
+ * @param a {number} - side of the rhombus A
+ * @param h {number} - height of the rhombus which is lowered to the side A
+ * @returns {number}
+ */
+function areaOfRhombus(a, h) {
+    return a * h;
+
+}
+
+
+/**
+ *
+ * @param r {number} - radius  of the cylinder r
+ * @param h {number} - height of the cylinder h
+ * @returns {number}
+ */
+function areaOfCylinder(r, h) {
+    return (2*3.14*(r*r))+(2*3.14*r*h);
+
+
+}
+
+/**
+ *
+ * @param a {number} - side of the triangle A
+ * @param h {number} - height of the triangle which is lowered to the side A
+ * @returns {number}
+ */
+function areaOfTriangle(a, h) {
+    return 1 / 2 * a * h;
+
+}
+
+/**
+ *
+ * @param a {number} - side of the rectangle A
+ * @param b {number} - side of the rectangle B
+ * @returns {number}
+ */
+function areaOfRectangle(a, b) {
+    return a * b;
+
+}
